195816c8dffd8ba88bc063e65120c47c32ed5c91
lbella <lbella@thoughtworks.com>
2018-04-15 16:15:43 -0300

    Exercise 1

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,13 @@
-// Faça o exercício dos parágrafos aqui
-// Este arquivo ainda não está incluído no arquivo HTML
+let botoesExpand = document.querySelectorAll('.botao-expandir-retrair');
+
+for(let botao of botoesExpand){
+	botao.addEventListener('click', function(e){
+		if(botao.parentNode.classList.contains('expandido')){
+			botao.parentNode.classList.remove('expandido');
+			botao.innerHTML = "+";
+		} else{
+			botao.parentNode.classList.add('expandido');
+			botao.innerHTML = "-";
+		}
+	});
+}
